6cba1aa996fed18caedbe2777840959697b1f223
mateusVedoy <mateusvedoy@outlook.com>
2022-08-18 22:08:09 -0300

    User Entity workflows discarded

--- src/app/use-cases/CreteUserService.ts
+++
@@ -1,8 +0,0 @@
-import { ICreateUserService } from "../../app/ports/use-cases/ICreateUser";
-import { User } from "domain/User";
-
-export class CreateUserService implements ICreateUserService {
-    create(login: string, pass: string): User {
-        throw new Error("Method not implemented.");
-    }
-}
