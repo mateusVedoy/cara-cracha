fa54c49751c5624c2965f21636c57749be392266
mateusVedoy <mateusvedoy@outlook.com>
2022-08-22 20:42:17 -0300

    Adapting timezone to BR - rollback

--- src/domain/Token.ts
+++ src/domain/Token.ts
@@ -33,8 +33,8 @@ export class Token {
         const day = date.getDate().toString().padStart(2, '0')
         const month = (date.getMonth() + 1).toString().padStart(2, '0')
         const year = date.getFullYear()
-        const hour = (date.getHours() - 3).toString().padStart(2, '0');
-        const min = date.getMinutes().toString().padStart(2, '0');
+        const hour = (date.getHours()).toString().padStart(2, '0');
+        const min = (date.getMinutes()).toString().padStart(2, '0');
         const formatted = `${year}-${month}-${day}T${hour}:${min}`;
         return formatted;
     }
