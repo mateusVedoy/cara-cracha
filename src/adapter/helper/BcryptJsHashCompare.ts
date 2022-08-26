18e04b8b66858d052bef8eac7517a2e23fc6f2bb
mateusVedoy <mateusvedoy@outlook.com>
2022-08-25 23:38:27 -0300

    update on Validate Token use case

--- src/adapter/helper/BcryptJsHashCompare.ts
+++ src/adapter/helper/BcryptJsHashCompare.ts
@@ -1,7 +1,7 @@
 import { IHashCompare } from "../../app/ports/helper/IHashCompare";
 import bcryptjs from "bcryptjs"
 
-export class BcryptJsPassCompare implements IHashCompare {
+export class BcryptJsHashCompare implements IHashCompare {
     public async compare(pass: string, hashedPass: string): Promise<Boolean> {
         return await bcryptjs.compare(pass, hashedPass);
     }
--- src/main/ValidateToken.ts
+++ src/main/ValidateToken.ts
@@ -2,13 +2,13 @@ import { ValidateTokenController } from "../adapter/controller/ValidateTokenCont
 import { ValidateTokenService } from "../app/use-cases/ValidateTokenService";
 import { Intercept } from "../adapter/middleware/Intercept";
 import { DecodeJWTToken } from "../adapter/helper/DecodeJWTToken";
-import { BcryptJsPassCompare } from "../adapter/helper/BcryptJsHashCompare";
+import { BcryptJsHashCompare } from "../adapter/helper/BcryptJsHashCompare";
 import { EmptyPropsValidate } from "../adapter/helper/EmptyPropsValidate";
 
 const intercepMiddleware = new Intercept();
 const decodeJWTToken = new DecodeJWTToken();
 const emptyPropsValidation = new EmptyPropsValidate();
-const bcryptJsHashCompare = new BcryptJsPassCompare();
+const bcryptJsHashCompare = new BcryptJsHashCompare();
 const validateTokenService = new ValidateTokenService(decodeJWTToken, bcryptJsHashCompare);
 const validateTokenController = new ValidateTokenController(validateTokenService, emptyPropsValidation);
 
