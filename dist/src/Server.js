27cb41b0f726a18d3b525057093064da47d68c1d
mateusVedoy <mateusvedoy@outlook.com>
2022-08-26 17:04:46 -0300

    develop three update

--- dist/src/Server.js
+++ dist/src/Server.js
@@ -1,7 +1,12 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
 const App_1 = require("./App");
 const Router_1 = require("./adapter/router/Router");
+dotenv_1.default.config();
 const PORT = process.env.PORT || 3001;
 const HOST = process.env.HOST || "0.0.0.0";
 const app = new App_1.App();
