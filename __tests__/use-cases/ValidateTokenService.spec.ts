e735e246166f4e470773ddc504819dc1fa193979
mateusVedoy <mateusvedoy@outlook.com>
2022-08-31 16:34:25 -0300

    Validate Token use-case unit test update

--- __tests__/use-cases/ValidateTokenService.spec.ts
+++ __tests__/use-cases/ValidateTokenService.spec.ts
@@ -8,9 +8,10 @@ describe("Validate Token use case", () => {
     let isTokenValid: Boolean;
     let invalidToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXNzIjoiJDJhJDEwJGJFM0h0MzlROENzYzd4Y2FTbGRPQU96OXhrRlFhNWhHbm1tTjB2d0hLMk8xTHF4WTdkb3YuIiwibG9naW4iOiJtZ29lcyIsImtleSI6IiQyYSQxMCQvT2ZieVRUU0pXSmFGY0MxSFpCVWt1VGlQZTFQaGhlSS9jRjhjSGgvdzRYL1VseWlVaHhLRyIsImlhdCI6MTY2MTQ2MTk0MiwiZXhwIjoxNjYxNDYzNzQyfQ.XEyJyqaJnbX3XVmDSWBKzqM3QowcH5-F8jarea6TQCY';
     let invalidKey = '00056943-3b47-4c39-b972-b477772c430c';
+    let data: Object = { login: '123456', pass: 'mgoes' };
 
     beforeEach(async () => {
-        generateToken = await createTokenService.create('123456', 'mgoes');
+        generateToken = await createTokenService.create(data);
     });
 
     it("should return true when token and key are valid", async () => {
